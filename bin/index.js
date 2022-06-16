60c96809466f9b384b41d5604c46cd6004501433
John Greer <johnmorrisgreer@gmail.com>
2022-06-15 21:13:22 -0400

    Fetches cat fats from an API

--- bin/index.js
+++ bin/index.js
@@ -1,21 +1,49 @@
 #!/usr/bin/env node
 
 const yargs = require("yargs")
+const axios = require("axios")
 
 const options = yargs
     .usage("Usage: -n <name>")
     .option("n", {
-         alias: "name", 
-         describe: "Your name", 
-         type: "string",
-         demandOption: true
+        alias: "name",
+        describe: "Your name",
+        type: "string",
+        demandOption: true
         }
-     )
+    )
     .argv
 
-const greeting = `Hello ${options.name}!`
-
+const greeting = `Hello, ${options.name}!`
 console.log(greeting)
+
+console.log("Here's a random joke for you:")
+
+const url = "https://cat-fact.herokuapp.com/facts"
+
+const getCatFacts = async () => {
+    const res = await axios.get( url, { headers: { Accept: "application/json" } } )
+    console.log(res.data[0].text)
+}
+
+getCatFacts()
+
+// const yargs = require("yargs")
+
+// const options = yargs
+//     .usage("Usage: -n <name>")
+//     .option("n", {
+//          alias: "name", 
+//          describe: "Your name", 
+//          type: "string",
+//          demandOption: true
+//         }
+//      )
+//     .argv
+
+// const greeting = `Hello ${options.name}!`
+
+// console.log(greeting)
 
 // const chalk = require("chalk")
 // const boxen = require("boxen")
