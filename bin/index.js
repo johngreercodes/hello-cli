80c43c1f66d0eaaebbe45619197dcecc9bee2dac
John Greer <johnmorrisgreer@gmail.com>
2022-06-15 20:35:37 -0400

    accepting arguments and talking back

--- bin/index.js
+++ bin/index.js
@@ -1,17 +1,34 @@
 #!/usr/bin/env node
 
-const chalk = require("chalk")
-const boxen = require("boxen")
+const yargs = require("yargs")
 
-const greeting = chalk.white.bold("Hello!")
+const options = yargs
+    .usage("Usage: -n <name>")
+    .option("n", {
+         alias: "name", 
+         describe: "Your name", 
+         type: "string",
+         demandOption: true
+        }
+     )
+    .argv
 
-const boxenOptions = {
-    padding: 1,
-    margin: 1,
-    borderStyle: "round",
-    borderColor: "green",
-    backgroundColor: "#555555"
-}
-const msgBox = boxen (greeting, boxenOptions)
+const greeting = `Hello ${options.name}!`
 
-console.log(msgBox)
+console.log(greeting)
+
+// const chalk = require("chalk")
+// const boxen = require("boxen")
+
+// const greeting = chalk.white.bold("Hello!")
+
+// const boxenOptions = {
+//     padding: 1,
+//     margin: 1,
+//     borderStyle: "round",
+//     borderColor: "green",
+//     backgroundColor: "#555555"
+// }
+// const msgBox = boxen (greeting, boxenOptions)
+
+// console.log(msgBox)
